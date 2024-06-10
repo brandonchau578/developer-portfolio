211624b4b173c9be2b0ba7d7caeabee0fc4de768
Brandon Chau <brandon.chau578@gmail.com>
2024-06-10 13:51:20 -0700

    :wq
    :WQ

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Brandon Chau",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "My name is Brandon Chau. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "My name is Brandon Chau. I am an instructor for Dreams For Schools. On my free time I love playing basketball, tennis and video games!",
   email: 'brandon.chau578@gmail.com',
   phone: '(626) 283-8223',
   address: 'Irvine, CA',
